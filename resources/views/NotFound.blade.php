492c32dcae704908769de16036831488b41f173f
codert0109 <thebluemoonsea@gmail.com>
2023-01-24 21:50:31 +0100

    add not found page

--- resources/views/NotFound.blade.php
+++ resources/views/NotFound.blade.php
@@ -1,8 +1,10 @@
 <html>
-    <body>
-        <h1>
-            Kidding Me
-        </h1>
-        <a href="{{env('DOMAIN_URL')}}"> Check</a>
-    </body>
+
+<body>
+    <h1>
+        Not Found Url
+    </h1>
+    <a href="{{env('DOMAIN_URL')}}">Return Home</a>
+</body>
+
 </html>
